2f57094e19b848cb91308c3cd1df1fa87d08c46c
intteliHome <iseoulyou@gmail.com>
2023-04-25 01:28:15 +0900

    paing update

--- src/main/java/org/zerock/guestbook2/controller/GuestbookController.java
+++ src/main/java/org/zerock/guestbook2/controller/GuestbookController.java
@@ -1,19 +1,56 @@
 package org.zerock.guestbook2.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.guestbook2.dto.GuestbookDTO;
+import org.zerock.guestbook2.dto.PageRequestDTO;
+import org.zerock.guestbook2.service.GuestbookService;
 
 @Controller
 @RequestMapping({"/guestbook"})
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-   @GetMapping({"/list","/"})
-   public String list(){
+    private final GuestbookService guestbookService;
 
-       return "/guestbook/list";
+    @GetMapping({"/"})
+    public String index(){
+
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+    
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        
+        log.info("dto... " + dto);
+        
+        // 새로 추가된 엔티티의 번호
+        Long gno = guestbookService.register(dto);
+        
+        redirectAttributes.addFlashAttribute("msg", gno);
+        
+        return "redirect:/guestbook/list";
+    }
+
+   @GetMapping({"/list"})
+   public void list(PageRequestDTO pageRequestDTO, Model model){
+
+        log.info("list............" + pageRequestDTO);
+
+       model.addAttribute("result", guestbookService.getList(pageRequestDTO));
    }
 
 }
