4b0fff03a6ac21e2e580145d00f1811452c01eae
intteliHome <iseoulyou@gmail.com>
2023-04-24 19:00:09 +0900

    secont

---
+++ src/main/java/org/zerock/guestbook2/service/GuestbookService.java
@@ -0,0 +1,37 @@
+package org.zerock.guestbook2.service;
+
+import org.zerock.guestbook2.Entity.Guestbook;
+import org.zerock.guestbook2.dto.GuestbookDTO;
+import org.zerock.guestbook2.dto.PageRequestDTO;
+import org.zerock.guestbook2.dto.PageResultDTO;
+
+public interface GuestbookService {
+
+    Long register(GuestbookDTO dto);
+
+    PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto){
+
+        Guestbook entity = Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+        return entity;
+    }
+
+    default GuestbookDTO entityToDto(Guestbook entity){
+
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regDate(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
+    }
+}
---
+++ src/main/java/org/zerock/guestbook2/service/GuestbookServiceImpl.java
@@ -0,0 +1,51 @@
+package org.zerock.guestbook2.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.zerock.guestbook2.Entity.Guestbook;
+import org.zerock.guestbook2.dto.GuestbookDTO;
+import org.zerock.guestbook2.dto.PageRequestDTO;
+import org.zerock.guestbook2.dto.PageResultDTO;
+import org.zerock.guestbook2.repository.GuestbookRepository;
+
+import java.util.function.Function;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class GuestbookServiceImpl implements GuestbookService{
+
+    private final GuestbookRepository repository;
+    @Override
+    public Long register(GuestbookDTO dto) {
+
+        log.info("DTO .............");
+        log.info(dto);
+
+        Guestbook entity = dtoToEntity(dto);
+
+        log.info(entity);
+
+        repository.save(entity);
+
+        return entity.getGno();
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook, GuestbookDTO> fn = (entity  -> entityToDto(entity));
+
+        return new PageResultDTO<>(result, fn);
+    }
+
+
+}
